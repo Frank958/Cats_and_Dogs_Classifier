2c7c0baf0895fb2fb4ab2eefe5901110a9d74a9b
Pham Phuc <phuc.ph9598@gmail.com>
2021-07-25 21:33:46 +0700

    here goes the message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 import cv2
+import seaborn as sns
+from sklearn.model_selection import train_test_split
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -18,15 +20,18 @@ from keras.layers import Flatten
 train_dir = 'C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\train\\train'
 test_dir = 'C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\test1\\test1'
 
-train_dogs = ['C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\train\\train\\{}'.format(i) for i in os.listdir(train_dir) if 'dog' in i]
-train_cats = ['C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\train\\train\\{}'.format(i) for i in os.listdir(train_dir) if 'cat' in i]
+train_dogs = ['C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\train\\train\\{}'.format(i) for i in
+              os.listdir(train_dir) if 'dog' in i]
+train_cats = ['C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\train\\train\\{}'.format(i) for i in
+              os.listdir(train_dir) if 'cat' in i]
 
-test_imgs = ['C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\test1\\test1\\{}'.format(i) for i in os.listdir(test_dir)]
+test_imgs = ['C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\test1\\test1\\{}'.format(i) for i in
+             os.listdir(test_dir)]
 
 train_imgs = train_dogs[:2000] + train_cats[:2000]
 random.shuffle(train_imgs)
 
-for ima in train_imgs[0:3]:
+for ima in train_imgs[0:5]:
     img = mpimg.imread(ima)
     imgplot = plt.imshow(img)
     plt.savefig('1.png')
@@ -47,8 +52,24 @@ def read_and_process_image(list_of_images):
         elif 'cat' in image:
             y.append(0)
 
-    return x,y
+    return x, y
+
 
 x, y = read_and_process_image(train_imgs)
 
-print(y[0])
+plt.figure(figsize=(20, 10))
+columns = 5
+for i in range(columns):
+    plt.subplot(2, columns, i + 1)
+    plt.imshow(x[i])
+    plt.savefig('2.png')
+
+x = np.array(x)
+y = np.array(y)
+
+sns.displot(y)
+plt.title('Labels for Cats and Dogs')
+plt.savefig('3.png')
+
+x_train, x_val, y_train, y_val = train_test_split(x,y, test_size=0.2,random_state=2)
+print(x_val.shape)
