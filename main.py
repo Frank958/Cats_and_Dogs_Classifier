59d595bc232289c15e037387d00208e263c7a5fd
Frank958 <81076279+Frank958@users.noreply.github.com>
2021-07-25 22:47:58 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import keras.models
+import numpy
 import pandas as pd
 import numpy as np
 import cv2
@@ -11,11 +13,11 @@ import os
 import random
 import gc
 
-from keras.models import Sequential
-from keras.layers import Convolution2D
-from keras.layers import MaxPooling2D
-from keras.layers import Dense
-from keras.layers import Flatten
+from keras import layers
+from keras import models
+from keras import optimizers
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array, load_img
 
 train_dir = 'C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\train\\train'
 test_dir = 'C:\\Users\\phucp\\PycharmProjects\\Cats and Dogs Classifier\\test1\\test1'
@@ -71,5 +73,42 @@ sns.displot(y)
 plt.title('Labels for Cats and Dogs')
 plt.savefig('3.png')
 
-x_train, x_val, y_train, y_val = train_test_split(x,y, test_size=0.2,random_state=2)
-print(x_val.shape)
+x_train, x_val, y_train, y_val = train_test_split(x, y, test_size=0.2, random_state=2)
+ntrain = len(x_train)
+nval = len(x_val)
+
+batch_size = 32
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
+model.add(layers.Dense(512, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer=optimizers.rmsprop(lr=1e-4), metrics=['acc'])
+
+train_datagen = ImageDataGenerator(rescale=1. / 255,
+                                   rotation_range=40,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True, )
+val_datagen = ImageDataGenerator(rescale=1. / 255)
+
+train_generator = train_datagen.flow(x_train, y_train, batch_size=batch_size)
+val_generator = val_datagen.flow(x_val, y_val, batch_size=batch_size)
+
+history = model.fit(train_generator,
+                    steps_per_epoch=ntrain // batch_size,
+                    epochs=64,
+                    validation_data=val_generator,
+                    validation_steps=nval // batch_size)
